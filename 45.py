7e9118b9475eab9a23b2bebc4c8c55cfc4ba310d
TainanKyle <kylehuang121@gmail.com>
2024-12-02 00:03:17 +0800

    step45 update 3D points

--- 45.py
+++ 45.py
@@ -44,10 +44,12 @@ def triangulation(pt1, pt2, P1, P2):
 def find_P2(pts1, pts2, P1, P2s):
     max_front_points = 0
     best_P2 = None
+    best_3D_points = []
 
     for P2 in P2s:
         # calculate 3D points that are in front of the cameras
         count = 0
+        cur_3D_points = []
         for pt1, pt2 in zip(pts1, pts2):
             X = triangulation(pt1, pt2, P1, P2)
 
@@ -56,13 +58,15 @@ def find_P2(pts1, pts2, P1, P2s):
             z2 = (P2 @ X)[2]
             if z1 > 0 and z2 > 0:
                 count += 1
+                cur_3D_points.append(X)
 
         # find the biggest count
         if count > max_front_points:
             max_front_points = count
             best_P2 = P2
+            best_3D_points = cur_3D_points
     
-    return best_P2
+    return best_P2, best_3D_points
 
 
 def step45(F, pts1, pts2, K1, K2):
@@ -71,5 +75,5 @@ def step45(F, pts1, pts2, K1, K2):
     P1 = np.hstack((np.eye(3), np.zeros((3, 1))))
     P2s = cal_P_options(E)
 
-    P2 = find_P2(pts1, pts2, P1, P2s)
-    return P1, P2
+    P2, points_3D = find_P2(pts1, pts2, P1, P2s)
+    return P1, P2, points_3D
